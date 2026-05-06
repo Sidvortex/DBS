fe7c48a442dc6c5b459ac723ef66b48ce6950b9a
sidvortex <ravadasiddharth@gmail.com>
2026-05-07 01:30:54 +0530

    info board

--- old version/predictor/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views  # ✅ Correct import from `predictor`
-
-urlpatterns = [
-    path('index/', views.index, name='index'),  # ✅ Make sure `index()` exists in `views.py`
-]
